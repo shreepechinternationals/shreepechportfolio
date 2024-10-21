5607692e533f0991ba2b608c5f411fb3e9d72240
Saran Adhikari <saranadhikari02@gmail.com>
2024-10-21 15:52:20 +0545

    added necessary plugins

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  base: '/shreepechportfolio/',  // Replace 'your-repo-name' with the actual name of your GitHub repository
+  build: {
+    outDir: 'dist'  // This ensures the build output goes to 'dist' (default for Vite)
+  }
+}
+
